0d1eb90d1bbf28777a805c5037bf08d3c25a8a70
jaeyoung.choi <lactrious@gmail.com>
2021-03-20 10:03:40 +0900

    Add sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,7 @@ module.exports = {
     "gatsby-plugin-typescript",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet"
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap"
   ]
 };
